abe3f1fae515adfab9b37b89d64f33b6bdc1dfda
maimonah95 <mim95s@hotmail.com>
2020-03-03 06:39:17 +0300

    solve hw-w07d02

--- models/vampire.js
+++ models/vampire.js
@@ -2,32 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
   // add your code here to set up your schema
-
-  const vampireSchema = new Schema({
-      name: String,
+  const vampireSchema = new Schema(
+    {
+      name: { type: String, required: true },
       title: String,
-      hair_color: String,
+      hair_color: { type: String, default: 'blonde'},
       eye_color: String,
       dob: Date,
       loves: [],
       location: String,
       gender: String,
-      victims: Number
+      victims: { type: Number, min: 0}
     },
     { timestamps: true }
   );
-//   var vampire = {
-//   name: 'Chocula',
-//   title: 'Count',
-//   hair_color: 'brown',
-//   eye_color: 'brown',
-//   dob: new Date(1971, 2, 13, 7, 47),
-//   loves: ['cereal', 'marshmallows'],
-//   location: 'Minneapolis, Minnesota, US',
-//   gender: 'm',
-//   victims: 2
-// };
-
 const Vampire = mongoose.model('Vampire', vampireSchema);
 
 //make this exportable to be accessed in `app.js`

925518fa5d0b1d2f8d2dd74929b29a0a8b332b00
RanenDev <github/RanenDev>
2020-03-03 08:07:46 +0300

    solve homework

--- models/vampire.js
+++ models/vampire.js
@@ -3,6 +3,15 @@ const Schema = mongoose.Schema;
 
 const vampireSchema = new Schema({
   // add your code here to set up your schema
+  name:{type :String ,required: true} ,
+  title: String,
+  hair_color: { type: String, default: 'blonde'},
+  eye_color: String,
+  dob:  { type: Date },
+  loves: [String],
+  location: { type: String },
+  gender: String,
+  victims:Number,min:0
 });
 
 const Vampire = mongoose.model('Vampire', vampireSchema);
